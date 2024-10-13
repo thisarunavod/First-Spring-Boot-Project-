b8137de3fdc21f0637e78ae131c4851fb4e6f6bc
thisaru <amithawerahena@gmail.com>
2024-10-13 11:51:00 +0530

    create config package

---
+++ src/main/java/lk/ijse/NoteTakerV2/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package lk.ijse.NoteTakerV2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Value("${secure.basic.username}")
+    String username;
+    @Value("${secure.basic.password}")
+    String password;
+    @Value("${secure.basic.role}")
+    String role;
+
+    @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //builder design
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+
+   }
+
+   //Create in memory user
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails principleUser = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles(role)
+                .build();
+        return new InMemoryUserDetailsManager(principleUser);
+
+    }
+}
